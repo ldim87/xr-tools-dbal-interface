e448d28f1a56b737f416ce21f7ea6c0332f1f46d
Admin <support@1000.menu>
2019-12-18 18:24:31 +0100

    Add files

---
+++ src/DatabaseManager.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Dmitriy Lukin <lukin.d87@gmail.com>
+ */
+
+namespace XrTools;
+
+/**
+ * Custom DBAL Interface
+ */
+interface DatabaseManager {
+
+	public function query(string $query, array $params = null, array $opt = []);
+
+	public function fetchColumn(string $query, array $params = null, array $opt = []);
+
+	public function fetchRow(string $query, array $params = null, array $opt = []);
+
+	public function fetchArray(string $query, array $params = null, array $opt = []);
+
+	public function connect(array $settings);
+	
+}
