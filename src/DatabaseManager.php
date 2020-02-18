c2ebfa00a7a299e2038907321e8868f0bc163d9a
Admin <support@1000.menu>
2020-02-18 22:09:58 +0100

    Add methods

--- src/DatabaseManager.php
+++ src/DatabaseManager.php
@@ -20,7 +20,13 @@ interface DatabaseManager {
 	
 	public function fetchArrayWithCount(string $query, array $params = null, array $opt = []);
 
-	public function connect(array $settings);
+	public function setConnectionParams(array $settings);
+
+	public function getAffectedRows();
+
+	public function getDebugMessages();
+
+	public function getQueryCollection();
 	
 	/**
 	 * MySQL transaction start
@@ -36,5 +42,13 @@ interface DatabaseManager {
 	 * MySQL transaction commit
 	 */
 	public function commit();
-	
+
+	/**
+	 * Insert / Update table via params
+	 * @param array  $data       Table data
+	 * @param string $table_name Table name
+	 * @param mixed  $index      Table update key (id or opt.index_key)
+	 * @param array  $opt        Options
+	 */
+	public function set(array $data, string $table_name, $index = null, array $opt = []);
 }
