be332273ae1046ba3d3f231172626374609d68da
Admin <support@1000.menu>
2020-04-10 16:01:13 +0200

    Add Connection Exception

---
+++ src/DBConnectionException.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author Dmitriy Lukin <lukin.d87@gmail.com>
+ */
+
+namespace XrTools;
+
+/**
+ * PDO Mysql Adapter for \XrTools\DatabaseManager Interface
+ */
+class DBConnectionException extends \Exception {
+
+}
